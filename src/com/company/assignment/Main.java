6e8107f88d7e0455a352afa70fc923ae281d5cc9
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-23 22:58:19 +1100

    Relational and logical operators in Java

---
+++ src/com/company/assignment/Main.java
@@ -0,0 +1,78 @@
+package com.company.assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    /*
+        ==== Relational Operators ====
+        - == comparison, not assignment
+        - != not equal
+        - > greater than
+        - < less than
+        - >= greater than of equal
+        - <= less than or equal
+
+        ==== Logical Operators =====
+        - AND ( && ) == both sides need to be true in order for the whole expression to return true
+        - OR ( || )
+        - NOT ( ! )
+
+     */
+
+//    double firstNum = 10;
+//    double secondNum = 3;
+//    double result = 0;
+//
+//    boolean isOld = true;
+//    boolean isYoung = false;
+//
+//    if((isOld && !isYoung) && (firstNum < secondNum)){
+//
+//        System.out.println("Horray");
+//
+//    } else {
+//
+//        System.out.println("Not right");
+//
+//    }
+
+
+
+
+//    result = firstNum % secondNum;
+//
+//    // If statements
+//        if (firstNum <= secondNum) {
+//            System.out.println("The remainder is " + result );
+//        } else {
+//            System.out.println("Numbers aren't equal");
+//        }
+
+        /*
+         for loop
+         while loop
+         */
+//        for ( int i = 0; i < 20; i++ ) {
+//
+//            //remainder (%) display numbers that are divisible by 2 or multiples of 2
+//            if ( i % 4 == 0 ) {
+//                System.out.println( i + " is a multiple of 4 " );
+//            }
+//        }
+
+
+//boolean return : as long as condition in while () is true expression in {} brackets will run
+        int i = 0;
+        while ( i < 5) {
+
+            System.out.println(" Counting ... " + i);
+            i++;
+        }
+
+
+    }
+
+
+}
